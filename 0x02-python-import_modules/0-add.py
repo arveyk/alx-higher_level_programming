c1fffdee4eb54d6c008526a0717358504b22d1de
CHARLES G <harveykisiangani@gmail.com>
2023-03-10 12:43:45 +0300

    main function added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 
+import add_0
+
 a = 1
 b = 2
 
+def addi(a, b):
+    plus = add_0.add(a, b)
+    print("{}".format(plus))
+
+def main():
+    addi(a, b)
+
 if __name__ == "__main__":
-    import add_0
-    print("{}".format(add_0.add(a, b)))
+    main()
 
