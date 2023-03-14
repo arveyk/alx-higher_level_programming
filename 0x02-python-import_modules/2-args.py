4f6a883aa0c70d6e4224e219e4dc9b63c4380145
CHARLES G <harveykisiangani@gmail.com>
2023-03-14 17:49:50 +0300

    list of commnd line args and count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,11 @@ def main():
     n = len(sys.argv)
     if n == 1:
         print("0 arguments")
+    else:
+        if n == 2:
+            print("{} argument".format(n - 1))
+        else:
+            print("{} arguments:".format(n - 1))
     for i in range(1, n):
         print("{}: {}".format(i, sys.argv[i]))
 
