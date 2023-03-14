d8fbbb0cad71f98f0779dfa0e6f9ff75bdf790ed
CHARLES G <harveykisiangani@gmail.com>
2023-03-14 12:04:09 +0300

    empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,9 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         b = None
+        tup = (0, b)
+        return tup
     else:
         b = sentence[0]
-    tup = (len(sentence), b)
-    return tup
+        tup = (len(sentence), b)
+        return tup
