b39c183ea7e6e3a22013cf34f16eb156d70411dd
CHARLES G <harveykisiangani@gmail.com>
2023-03-20 16:40:07 +0300

    pycode style

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+
 for i in range(10):
     for p in range(9):
-         print(" {},".format(i), ' ', end="")
-    print(" {},".format(i), ' ', end="")
+        if i == p or i*10 + p == 10*p + i:
+            continue
+        print("{}{}".format(i, p), end="")
+        if i < 9 or p < 9:
+            print(",", end=" ")
+print()
