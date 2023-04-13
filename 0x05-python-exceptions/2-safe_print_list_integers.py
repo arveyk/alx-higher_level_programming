b944d147c62e1a23dd969ff4fa68600c53c737da
CHARLES G <harveykisiangani@gmail.com>
2023-04-13 14:30:49 +0300

    safe print division

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    c = 0
     try:
-        c = 0
+        i = 1
         for i in range(x):
-            if ((ord(i) >= 65 and ord(i) <= 90) or \
-                    (ord(i) >= 97 and ord(i) <= 122)):
-                continue
             c += 1
             print("{:d}".format(my_list[i]), end="")
         print()
-        return c
-    except IndexError:
-        print()
+        return (c)
     except TypeError:
-        print()
+        pass
+    except ValueError:
+        pass
