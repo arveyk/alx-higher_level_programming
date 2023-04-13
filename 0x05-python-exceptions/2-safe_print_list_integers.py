71c5e391b14d0575ec4193727b0df559488016a0
CHARLES G <harveykisiangani@gmail.com>
2023-04-13 17:06:24 +0300

    raising exceptions + messages

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,14 @@
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
     try:
-        i = 1
         for i in range(x):
             c += 1
             print("{:d}".format(my_list[i]), end="")
         print()
-        return (c)
-    except TypeError:
-        pass
+        return c
+
     except ValueError:
-        pass
+        c -= 1
+    except TypeError:
+        c -= 1
+
--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -3,5 +3,5 @@ raise_exception = __import__('5-raise_exception').raise_exception
 
 try:
     raise_exception()
-exception TypeError as te:
+except TypeError as te:
     print("Exception raised")
