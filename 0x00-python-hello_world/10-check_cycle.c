411dce951f114a5c9b8cd8838e1cd5175a7c0b0b
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-08 00:09:12 +0300

    finding a cycle in a linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "list.h"
+#include <stdlib.h>
+
+/**
+ * check_cycle - checks for the existance of a cycle in a listint_t list
+ * @list: pointer to head node
+ *
+ * Return: 0 if no cycle, 1 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *trav = NULL;
+	listint_t *comp = NULL;
+
+	if (list == NULL)
+		return (0);
+	trav = list->next;
+	while (trav != NULL)
+	{
+		if (trav == list)
+			return (1);
+		comp = trav->next;
+		while (comp != NULL)
+		{
+			if (comp == trav)
+				return (1);
+			comp = comp->next;
+		}
+		trav = trav->next;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to the head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+
+	
+}
