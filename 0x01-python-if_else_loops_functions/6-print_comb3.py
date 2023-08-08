919a602701af3cbe041cba00ab1f01ba8148cfd0
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-09 01:10:22 +0300

    fair trial for cobi function

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for r in range(0, 10):
     for s in range(0, r):
-        if s == r:
+        if 10 * (s) + r == s + 10 * (r):
             continue
-        print ("{:d}{:d}".format(s, r), end="")
+        print("{:d}{:d}".format(s, r), end="")
         if s < 8:
             print(", ", end="")
 print()
