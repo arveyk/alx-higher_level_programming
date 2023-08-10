96037fa750ee40b8d62f1e4aaa29122aeaeff34c
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-10 13:22:22 +0300

    files created. functions imported

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__=="__main__":
+    y = len(argv) - 1
+    if y == 1:
+        print("{:d} argument:".format(y))
+        print("{:d}: {:s}".format(y, argv[y]))
+    elif y == 0:
+        print("{:d} arguments:".format(y))
+    else:
+        print("{:d} argument:".format(y))
+        for arg_s in range(1, y + 1):
+            print("{:d}: {:s}".format(arg_s, argv[arg_s]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__=="__main__":
+    sum_of = 0
+    y = len(argv)
+    for num in range(1, y):
+        sum_of += int(argv[num])
+    print("{:d}".format(sum_of))
