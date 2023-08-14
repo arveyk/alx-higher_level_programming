940d2e8bd08d6a6f1edf762a19daa439212475a1
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-15 00:38:24 +0300

    range pushed to the endge by 1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
-    y = len(my_list)
+    y = len(my_list) + 1
     x = 0
     for i in range(y):
         if x == idx:
