c51c37d94c2b4cc86ba7b35d9d4c2044b8c79134
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-14 23:36:02 +0300

    matrix print, differnt size accounted for

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
     list_b = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+        return list_b
     for h in list_b:
         if h == idx:
             list_b[idx] = element
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
+        z = 0
         for f in i:
             print("{:d}".format(f), end="")
-            if f % len(i) != 0:
+            z += 1
+            if z % len(i) != 0:
                 print(" ", end="")
         print()
