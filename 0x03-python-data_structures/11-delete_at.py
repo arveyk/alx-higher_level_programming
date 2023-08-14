aef904c386f778b28ef472b32141fd237a282e83
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-15 00:36:24 +0300

    indexing within loop, use different var

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,8 +5,9 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
     y = len(my_list)
+    x = 0
     for i in range(y):
-        if (i == idx):
-            del(my_list[idx])
-        i += 1
+        if x == idx:
+            del(my_list[x])
+        x += 1
     return my_list
