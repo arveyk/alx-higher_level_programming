ecc3272a49b5f7669e7db7efc568bc980f98b9cc
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-15 00:24:01 +0300

    correct indexing method for each element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,9 @@ def new_in_list(my_list, idx, element):
     list_b = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return list_b
+    in_lst = 0
     for h in list_b:
-        if h == idx:
+        if in_lst == idx:
             list_b[idx] = element
+        in_lst += 1
     return list_b
