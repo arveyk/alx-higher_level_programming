ac0194de053bf2903be1ef371351a579a21eea40
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-15 11:04:41 +0300

    correction on flow of logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,7 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_1 = len(tuple_a)
     len_2 = len(tuple_b)
     a, b, f, g = 0, 0, 0, 0
-    if len_1 == 1 and len_2 >= 2:
+    if len_1 == 1:
         a = tuple_a[0]
     elif len_1 >= 2:
         a, b = tuple_a[0], tuple_a[1]
