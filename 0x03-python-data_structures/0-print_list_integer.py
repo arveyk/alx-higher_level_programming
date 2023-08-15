e5117568212a8cd3844d390da805332f98d89f2c
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-15 11:23:16 +0300

    defining the indexing var before use

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,6 +5,6 @@ def print_list_integer(my_list=[]):
     if my_list is None:
         pass
     else:
+        a = len(my_list)
         for j in range(a):
-            z = int(my_list[j])
-            print("{:d}".format(z))
+            print("{:d}".format(my_list[j]))
