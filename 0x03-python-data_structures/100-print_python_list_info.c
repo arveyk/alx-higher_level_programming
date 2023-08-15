7304610ff326b5e20da44fa79511ffa9298c02be
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-15 10:44:59 +0300

    for list==NULL

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info
+ * @p: pointer to struct i guess
+ *
+ * Return: None
+ */
+void print_python_list(PyObject *p)
+{
+
+		printf("%s", p);
+}
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if len(my_list) == 0:
         print()
     else:
         my_list.reverse()
