62defa3a442ec3de6be27f4acea2abedcaeb1c78
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-16 23:40:12 +0300

    complex delete, dict has to be force into a list for delition during iteration to happen

--- 0x04-python-more_data_structures/102-main.py
+++ 0x04-python-more_data_structures/102-main.py
@@ -3,9 +3,8 @@ complex_delete = __import__('102-complex_delete').complex_delete
 print_sorted_dictionary = \
         __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]
-new_dict = complex_delete(a_dictionary, 'c')
-print_sorted_dictionary(a_dictionary, 'C'
+a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+new_dict = complex_delete(a_dictionary, 'C')
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
@@ -15,5 +14,5 @@ print("--")
 new_dict = complex_delete(a_dictionary, 'c_is_fun')
 print_sorted_dictionary(a_dictionary)
 print("--")
-print_sorted_dictionary(a_dictionary)
+print_sorted_dictionary(new_dict)
 
