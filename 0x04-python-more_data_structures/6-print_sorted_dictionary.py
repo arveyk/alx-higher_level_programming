9e63ec6e42bff12d2f071ca4b227e84823452ad8
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-16 21:50:30 +0300

    iterator removed, update dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,9 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    sort_of = sorted(a_dictionary)
-    for m in sort_of:
-        print("{:s}: {}".format(m, a_dictionary[m]))
+    if a_dictionary is None:
+        pass
+    else:
+        sort_of = sorted(a_dictionary)
+        for m in sort_of:
+            print("{:s}: {}".format(m, a_dictionary[m]))
