b90d1323c9fdd958dff53b71bab8d71d18423ab1
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-16 22:09:30 +0300

    del a key value

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+    if key is None:
+        pass
+    else:
+        if key in a_dictionary:
+            del(a_dictionary[key])
+    return a_dictionary
