98d3401059b239d109bcdcd056e079e4a5a17ff7
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-28 23:34:10 +0300

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def  safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+    except Exception as qw:
+        print("Exception: {}".format(qw), file=sys.stderr)
+        return None
+    return result
