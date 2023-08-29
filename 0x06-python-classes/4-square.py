ba2b10b037ca080cec997e73e6e9992e71186b19
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-30 00:39:54 +0300

    getter and setter properties

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,2 +1,47 @@
 #!/usr/bin/python3
-"""
+"""properties getters and setters demo"""
+
+
+class Square:
+    """computes area of a square"""
+    def __init__(self, size):
+        """
+        Args:
+            size: size of a square
+        Return:
+            No value
+        """
+        self.__size = size
+        """except Exception as e:
+            print("size must be an integer", e)
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        """
+    @property
+    def size(self):
+        """getter retrieves the value of size
+        Args:
+            value: size of a square
+        Returns:
+            dimensions of square
+        """
+        return self.__size
+    @size.setter
+
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        self.__size = value
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+
+    def area(self):
+        """area method computes the area of a square
+
+        Args:
+            None
+        Returns:
+            Area of a square
+        """
+        return self.__size ** 2
