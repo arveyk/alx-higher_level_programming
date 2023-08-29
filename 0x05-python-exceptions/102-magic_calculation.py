4214ac67d8eb3d9bcb16dedcab6490d504aa0191
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-29 12:17:43 +0300

    conditional statement block rightly indented

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,8 +3,8 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         if i > a:
+            result = a + b
             raise Exception('Toofar')
-        result = a + b
-    else:
-        result += (a ** b) / i
+        else:
+            result += (a ** b) / i
     return result
