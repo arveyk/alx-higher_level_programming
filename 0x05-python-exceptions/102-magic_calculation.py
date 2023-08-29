9ff4815375a61407903f280d8da2eec3bc2380d6
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-29 12:11:25 +0300

    pycodestyle

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,9 +2,9 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-            if i > a:
-                raise Exception('Toofar')
-                result = a + b
-            else:
-                result += (a ** b) / i
+        if i > a:
+            raise Exception('Toofar')
+        result = a + b
+    else:
+        result += (a ** b) / i
     return result
