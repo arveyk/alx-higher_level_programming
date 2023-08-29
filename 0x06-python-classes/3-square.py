fd34a05f5d01bcd402b11df9669c7cf4d2b4de56
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-29 23:37:18 +0300

    pycode, docstring for class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,11 +3,16 @@
 
 
 class Square:
+    """Square class create a type that computes
+    area of a square, when givena  size
+    """
     def __init__(self, size=0):
-        '''
+        """
         Args:
             size: size of a square
-        '''
+        Return:
+            No value
+        """
         try:
             self.__size = size
         except Exception as e:
