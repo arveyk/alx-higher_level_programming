e755aaf206a96716d8601db0c65c40f701d583f7
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-30 12:03:27 +0300

    required parameter included

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,7 +10,7 @@ class Square:
     """
     computes area of a square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Args:
             size: size of a square
