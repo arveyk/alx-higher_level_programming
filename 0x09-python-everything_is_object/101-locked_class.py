5a4189d8a6e64a3fd3495f9a9557312921d91d19
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-06 15:55:52 +0300

    Documentation added

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""Creating a Locked class
+    __slots__ is used to avoid dynamically created attributes
+"""
+
 class LockedClass(object):
+    """Creation of a Locked class, new instance attributes have to be
+        called first_name
+    """
 
     __slots__ = ['first_name']
 
     def __init__(self, first=None):
+        """Initializes the instance attributes,
+        Args:
+            first: name of the varible locked to first_name
+        Return: No value
+        """
         self.first_name = first
