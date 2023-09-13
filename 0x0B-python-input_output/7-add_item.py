1d01cd1f3054c0d51dfbe1796d6e43b23f7809a3
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-13 13:27:30 +0300

    add from command line

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,13 +10,9 @@ save_to_json = __import__('5-save_to_json_file').save_to_json_file
 load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
 save_list = []
-
-while s < args_len:
-    save_list.append(sys.argv[s])
-
-with open(0, encoding='utf-8') as cmd_line:
-    for i in cmd_line:
-        save_list.append(i)
-
-with open('add_item.json', mode="w", encoding='utf-8') as add_item:
-    json.dump(save_list, add_item)
+s = 1
+if args_len > 1:
+    while s < args_len:
+        save_list.append(sys.argv[s])
+        s += 1
+save_to_json(save_list, 'add_item.json')
