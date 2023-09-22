b2a9f9136e7007157bff9178d6766e016466ea8c
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-22 22:28:52 +0300

    exception string correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -52,7 +52,7 @@ class Rectangle(Base):
         if type(height) is not int:
             raise TypeError('height must be an integer')
         if height <= 0:
-            raise ValueError('height must be > 0')
+            raise ValueError('height must be >= 0')
         self.__height = height
 
     @property
@@ -68,7 +68,7 @@ class Rectangle(Base):
         if type(x) is not int:
             raise TypeError('x must be an integer')
         if x < 0:
-            raise ValueError('x must be > 0')
+            raise ValueError('x must be >= 0')
         self.__x = x
 
     @property
