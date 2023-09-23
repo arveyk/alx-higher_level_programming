e0c5c8edda9c9ba399a92fdc69ae2d408be80af5
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-23 23:29:25 +0300

    rectangle with area and str methods  implemented

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""inintillizes the dimantions of figure whose area is to be computed
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class defining a rectangle
+    """
+    def __init__(self, width, height):
+        """
+        Args:
+            width: how wide the rectangle is
+            height: how long the rectangle is
+
+        Returns: No return value
+        """
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """ returns the string representation of rectangle class
+            Args: none
+            Return: informal string representation of class rectangle
+        """
+        return str(f'[Rectangle] {self.__width}/{self.__height}')
+
+    def area(self):
+        """ Calculates the area of a rectangle
+            Args: No explicit return value
+            Returns: the area
+        """
+        return self.__width * self.__height
