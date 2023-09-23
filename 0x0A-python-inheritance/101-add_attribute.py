c529a765131b62e64ea87d03a85fa2c80f0fa20c
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-23 21:29:38 +0300

    Attribute added if possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,13 @@
 
 
 def add_attribute(obj, name, value):
+    """ add_attribute adds an instance attribute if and only if it 
+    is possible
+        Args:
+            obj: the object to have an additional attribute
+            name: name of the attribute
+            value: the value of the attribute
+        Return: No explicit return value
+    """
     if not (setattr(obj, name, value)):
         raise TypeError('can\'t add new attribute')
-
