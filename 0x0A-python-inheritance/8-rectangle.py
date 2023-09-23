ca9cfe9c38f2ad063b398530dc9b8c0c6399ed65
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-23 23:05:03 +0300

    rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,16 +2,17 @@
 """inintillizes the dimantions of figure whose area is to be computed
 """
 
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """class defining a rectangle
     """
     def __init__(self, width, height):
-        super().__init__()
+        """
 
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
-        return str('Rectangle({self.__width}, {self.__height})')
+        '''return str('Rectangle({self.__width}, {self.__height})')'''
