b7316905bf1753048c999b3b1afd3c111ab6d392
Harvey W. Kis <harveykisiangani@gmail.com>
2023-09-23 23:43:36 +0300

    documentation for class and methods

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,7 +4,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Class Square definition"""
     def __init__(self, size):
+        """ initialization for every square object
+            Args:
+                size: dimentions of the square
+            Returns: no explicit return value
+        """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
