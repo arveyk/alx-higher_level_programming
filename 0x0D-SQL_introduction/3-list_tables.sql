c43a8d24ce92fe60f016ab2a943d1bd48c98dde4
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-18 23:04:38 +0300

    var name no matter

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,7 @@
 -- Script for listing all tables of a database in MySQL server
 -- SET @var := CONCAT('SELECT * FROM information_schema.tables ');
-SET @var := CONCAT('SHOW TABLES FROM information_schema');
-PREPARE stmt FROM @var;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+-- SET @var := CONCAT('SHOW  TABLES FROM information_schema');
+-- PREPARE stmt FROM @var;
+-- EXECUTE stmt;
+-- DEALLOCATE PREPARE stmt;
+SHOW TABLES  FROM information_schema;
