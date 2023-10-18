69fa859d44b48cd707d445a60db813f824f9e5c7
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-18 23:37:14 +0300

    auto-increment primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Script to create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_od_usa; USE 	hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
