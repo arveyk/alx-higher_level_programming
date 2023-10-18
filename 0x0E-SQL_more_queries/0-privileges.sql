505c93ef240155d19445d3ec91746fc03f5ab3a0
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-18 22:26:33 +0300

    ; at end of each statement

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script for showing the permissions of some users
-SHOW GRANTS FOR 'user_0d_1'@'host'
-SHOW GRANTS FOR 'user_0d_2'@'host'
+SHOW GRANTS FOR 'user_0d_1'@'host';
+SHOW GRANTS FOR 'user_0d_2'@'host';
