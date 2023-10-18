c9c864ee0aeebea76a2f9d15c1138c182d6df901
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-18 18:16:59 +0300

    show tables, correct output

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,6 @@
 -- Script for listing all tables of a database in MySQL server
 -- SET @var := CONCAT('SELECT * FROM information_schema.tables ');
-SET @var := CONCAT('DESCRIBE information_schema.tables ');
+SET @var := CONCAT('SHOW TABLES FROM information_schema');
 PREPARE stmt FROM @var;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
