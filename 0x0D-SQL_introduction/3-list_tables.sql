a7897b26b96506d902e4ed4754b21522d80b5d70
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-18 18:05:02 +0300

    describe function

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,6 @@
 -- Script for listing all tables of a database in MySQL server
 -- SET @var := CONCAT('SELECT * FROM information_schema.tables ');
-SET @var := CONCAT('SELECT *  FROM information_schema.tables ');
+SET @var := CONCAT('DESCRIBE information_schema.tables ');
 PREPARE stmt FROM @var;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
