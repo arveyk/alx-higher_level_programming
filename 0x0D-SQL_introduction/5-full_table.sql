37013e239f749af100c77a022945b83f11b749b2
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-20 00:48:34 +0300

    4's implemetation

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
 -- Script to show full desc of table
-desc first_table FROM hbtn_0c_0;
+SET @var := CONCAT('desc first_table FROM hbtn_0c_0');
+PREPARE stmt FROM @var;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
