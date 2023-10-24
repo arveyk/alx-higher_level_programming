629aab7a285eb01324ebf2af02f6022d03ef860b
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-25 01:35:48 +0300

    convert to utf-6

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Script for converting DATABASE TO UTF-8
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table  MODIFY 
+name  VARCHAR(5)
+CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
