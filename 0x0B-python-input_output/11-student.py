760894cdb1f9a7075097aa405482f70b8bd3ad12
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-08 23:26:20 +0300

    std to disk and reloaded

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -21,10 +21,25 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attr=None):
         """return dictionary for JSON serialization of an object
         Args:
             obj: the class object
         Returns: the dictionary description
         """
-        return dict(self.__dict__)
+        new_dict = {}
+        if type(attr) is list:
+            for name in attr:
+                if name in self.__dict__:
+                    new_dict[name] = self.__dict__[name]
+            return (new_dict)
+        else:
+            return dict(self.__dict__)
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+        Args:
+            json - dictionary
+        Returns: None yet
+        """
+        new_dict = self.__dict__ = json
