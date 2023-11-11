f9b0895ff5f2bdfd4cfef406664932b69ae9d23e
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-11 19:22:04 +0300

    synatax corrected

--- 0x0E-SQL_more_queries/8-cities_of_californial_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_californial_subquery.sql
@@ -1,2 +1,4 @@
 -- Script that lists all cities of calif
-SELECT name FROM hbtn_0d_usa WHERE name=California ORDER BY ASC;
+SELECT name FROM hbtn_0d_usa
+WHERE name='California'
+ORDER BY cities.id ASC;
