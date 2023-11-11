c1a021606bd302295f26656be928bb839decbf65
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-11 19:05:30 +0300

    correct syntax for adding foregn and primary keys

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,10 @@
 -- Script that creates a database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; USE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id)REFERENCES states(id)
 );
