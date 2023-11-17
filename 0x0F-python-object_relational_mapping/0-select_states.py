c8a5a622be48fd6dbb48c7a8d61bb1b619626c9e
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-18 01:22:43 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         password = argv[2]
         db_name = argv[3]
 
-        db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=db_name)
+        db = MySQLdb.connect(user=username, passwd=password, db=db_name)
         c_ursor = db.cursor()
         c_ursor.execute("SHOW TABLES")
         #c_ursor.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES")
