06cd4a36057a912de189f116b34941cdcc4d1683
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-18 19:07:08 +0300

    filter by case

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Module to execute mysql command'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        argv = sys.argv
+        username = argv[1]
+        password = argv[2]
+        db_name = argv[3]
+
+        db = MySQLdb.connect(user=username, passwd=password, db=db_name)
+        c_ursor = db.cursor()
+        c_ursor.execute("SELECT * FROM states  WHERE name LIKE 'N%' ORDER BY states.id")
