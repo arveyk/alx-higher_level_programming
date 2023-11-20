992241f8ae67c76cabfe7b87a9f6e1583daed309
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-20 18:48:41 +0300

    key words capitalized

--- 0x0E-SQL_more_queries/10-genre_id_all_shows.sql
+++
@@ -1,4 +0,0 @@
--- Script to display all shows
-SELECT title, id as genre_id
-FROM tv_shows
-ORDER BY title, genre_id ASC;
--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Script to display all shows
-SELECT title, id as genre_id
+SELECT title, id AS genre_id
 FROM tv_shows
 ORDER BY title, genre_id ASC;
