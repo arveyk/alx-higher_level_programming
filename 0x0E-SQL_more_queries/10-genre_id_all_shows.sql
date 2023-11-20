ea5c983abff660d66328efe13ca0a27a3728072a
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-20 18:42:27 +0300

    tv show

--- 0x0E-SQL_more_queries/10-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/10-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- Script to display all shows
-SELECT tv_show.title, tv_shows_genres.genre_id
-FROM hbtn_0d_tvshows
-ORDER BY tv_shows.title ASC tv_shows_genres.genre_id DESC;
+SELECT title, id as genre_id
+FROM tv_shows
+ORDER BY title, genre_id ASC;
