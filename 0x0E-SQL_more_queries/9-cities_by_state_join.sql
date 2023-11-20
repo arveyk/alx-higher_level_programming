f7478d012078a1b8fae0faf6e55e8dec1ce8038d
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-20 18:18:59 +0300

    joining two tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,5 @@
 -- Script that lists all cities of calif
-SELECT cities.id, cities.name states.name 
-FROM hbtn_0d_usa WHERE cities.name=California ORDER BY ASC;
+SELECT cities.id, cities.name, states.name
+FROM states JOIN cities
+WHERE states.name = 'California'
+ORDER BY cities.id ASC;
