dddd908c610a23e4e5ea5bde875d37d6062d46e5
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-01 23:42:06 +0300

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 # Module for sending HTTP request
 import urllib.request
 
-#req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+# req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen('http://alx-intranet.hbtn.io/status') as resp:
     html = resp.read()
 
