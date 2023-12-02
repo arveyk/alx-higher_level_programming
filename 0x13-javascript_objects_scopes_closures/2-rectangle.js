c7771cf25aa272089ad20f6813b8eadc667fb5fa
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-02 23:28:48 +0300

    empty object created

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w === 0 || h === 0) || (w < 0 || h < 0)) {
-      this.width = {}
-      this.height ={}
+    if (!w || !h) {
+      let obj = {};
+    }
+    else if ((w === 0 || h === 0) || (w < 0 || h < 0)) {
+      let obj = {};
     }
     else {
       this.width = w;
       this.height = h;
-    }
+    } 
   }
 }
 
