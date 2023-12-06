1cfa2f90fd9a0f7468ca8948aade9f92b7d90335
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-07 00:21:35 +0300

    semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-let num  = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2]);
 let hash = 'X';
 
 if (!num) {
   console.log('Missing number of occurences');
 } else {
-  let count = num
+  let count = num;
   while (count > 1) {
-    hash += "X";
+    hash += 'X';
     count -= 1;
   }
   for (let num2 = num; num2 > 0; num2--) {
     console.log(hash);
   }
 }
-
