c3bbddbabe5e7f8916be8253c976ce757dcd7529
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-06 16:09:24 +0300

    inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script that lists all cities of calif
 SELECT cities.id AS id, cities.name AS name, states.name AS name
-FROM cities JOIN states
+FROM cities INNER JOIN states
 WHERE states.name = 'California'
-ORDER BY cities.id ASC;
+ORDER BY id ASC;
