ff36c44515588fe031f061d67b74affb0b0fa9f4
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-06 23:49:55 +0300

    correct position of args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const len = process.argv.length;
 
-if (len <= 1) {
+if (len <= 2) {
   console.log('No argument');
-} else if (len === 2) {
+}
+if (len == 3) {
   console.log('Argument found');
-} else if (len > 2) {
+}
+if (len > 3) {
   console.log('Arguments found');
 }
