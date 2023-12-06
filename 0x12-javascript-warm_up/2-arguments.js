9ece8573e249a5b04ad590c226edd8ca0a717b06
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-06 23:39:29 +0300

    process.argv.length used

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-let count = 0;
-const { argv } = require('node:process');
-argv.forEach((val, index) => {
-  count = index;
-});
-if (count <= 1) {
+const len = process.argv.length;
+
+if (len <= 1) {
   console.log('No argument');
-} else if (count === 2) {
+} else if (len === 2) {
   console.log('Argument found');
-} else if (count > 2) {
+} else if (len > 2) {
   console.log('Arguments found');
 }
