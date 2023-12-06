95f2872d340d88054093ab9083c9964bb83de09d
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-07 00:28:48 +0300

    add function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,27 +1,12 @@
 #!/usr/bin/node
-let count = 0;
-let num;
-let num1;
-const { argv } = require('node:process');
+const num = parseInt(process.argv[2]);
+const num1 = parseInt(process.argv[3]);
 
-
-argv.forEach((val, index) => {
-  count = index;
-  if (index == 2) {
-    num = Number(val);
-  }
-  if (index == 3) {
-    num1 = Number(val);
-  }
-});
-
-
-if (count !== 3) {
+if (!num1) {
   console.log('Missing number of occurences');
 } else {
   function add (num, num1) {
     return num1 + num;
-};
-  console.log(add(num + num1));
+  }
+  console.log(add(num, num1));
 }
-
