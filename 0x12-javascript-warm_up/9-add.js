c5b55c26bb18b0427ba4b18440065488024ecb98
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-07 13:18:46 +0300

    adding numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,7 @@
 const num = parseInt(process.argv[2]);
 const num1 = parseInt(process.argv[3]);
 
-if (!num1) {
-  console.log('Missing number of occurences');
-} else {
-  function add (num, num1) {
-    return num1 + num;
-  }
-  console.log(add(num, num1));
+function add (num, num1) {
+  return num1 + num;
 }
+console.log(add(num, num1));
