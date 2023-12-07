228a4e9b487ad5638df686d486838f4062fadbc0
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-07 13:33:37 +0300

    function to find second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,18 @@
 #!/usr/bin/node
-/*const { argv } = require('node:process'); */
-let count = 0;
-let num = 0;
-let num2 = 0;
-/*
-argv.forEach((val, index) => {
-  count = index;
-  if (count === 2) {
-    console.log(`${val}`);
-  }
-});
-*/
-if (count == 1) {
+let num = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+const count = process.argv.length;
+let trav = 2;
+
+if (!num || !num2) {
   console.log('0');
-} else while (count >= 2 ) {
-  if (Number(process.argv[count]) > num) {
-    num2 = num;
-    num = Number(process.argv[count]);
+} else {
+  while (trav < count) {
+    if ((parseInt(process.argv[trav])) > num) {
+      num2 = num;
+      num = parseInt(process.argv[trav]);
+    }
+    trav += 1;
   }
-  count -= 1;
+  console.log(num2);
 }
-console.log(num2);
