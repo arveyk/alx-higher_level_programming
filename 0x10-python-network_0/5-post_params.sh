84c39215ce7a46efbe07881b4fe650b3aef22d23
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-13 15:16:26 +0300

    single quotes removed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send post request
-curl -s -d "email:test@gmail.com&subject:'I will always be here for PLD'" "$1"
+curl -s -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
