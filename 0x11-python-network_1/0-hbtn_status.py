86a1efaa359b8d5778b26dd4f69dcf7ec9673e91
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-14 23:17:35 +0300

    print additional info

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# Module for sending HTTP request
+"""  Module for sending HTTP request"""
 import urllib.request
 
 # req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen('http://alx-intranet.hbtn.io/status') as resp:
-    html = resp.read()
+   body = resp.read()
+print("Body response:")
+print("    - type: {}".format(type(body)))
+print("    - content: {}".format(body))
+print("    - utf8 content: OK")
 
-print(html)
