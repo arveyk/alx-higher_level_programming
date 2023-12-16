c8d98dfb0189b9a5a11e75ba0e82a115502b6491
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-16 22:50:30 +0300

    adding % in print statement

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,15 +8,16 @@ if __name__ == '__main__':
     if len(sys.argv) == 5:
         argv = sys.argv
         usrname = argv[1]
-        password = argv[2]
+        pwd = argv[2]
         db_name = argv[3]
         stt = str(argv[4])
 
-        db = MySQLdb.connect(user=usrname, passwd=password, db=db_name)
+        db = MySQLdb.connect(user=usrname, port=3306, passwd=pwd, db=db_name)
         c_ursor = db.cursor()
-        stmt = """SELECT * FROM states WHERE name = %s  ORDER BY states.id"""
-        c_ursor.execute(stmt, (stt,))
+        #stmt = "SELECT * FROM states WHERE name = (%s)  ORDER BY states.id"
+        # c_ursor.execute(stmt)
+        c_ursor.execute("SELECT * FROM states WHERE name = (%s)  ORDER BY states.id", stt)
         rows = c_ursor.fetchall()
 
         for eachRow in rows:
-            print(eachRow)
+            print("%s" % eachRow)
