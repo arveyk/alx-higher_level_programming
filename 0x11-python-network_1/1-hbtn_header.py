cb3c5e98bd9d266bed53a54716d04fbe9e49c19a
@arveyk <harveykisiangani@gmail.com>
2023-12-18 14:39:51 -0800

    request.info

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(url) as resp:
-        hdr = resp.read()
-        print(hdr)
+        hdr = resp.info()
+        print(hdr["X-Request-Id"])
