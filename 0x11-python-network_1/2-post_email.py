9bfb6b19de80c4b7adb0d49e121b464f52732033
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-18 14:44:45 +0300

    Post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module to send a request and display the value of X-Request-Id
+"""
+import urllib.request
+import urllib.parse as parse
+import sys
+
+
+if __name__ == "__main__":
+    """ Only functions when not imported
+    """
+    url = sys.argv[1]
+    data_dict = {"email" : sys.argv[2]}
+    data = parse.urlencode(data_dict)
+    data = data.encode('ascii')
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as resp:
+        page = response.read().decode('utf-8')
+        print(page)
