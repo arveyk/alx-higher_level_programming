f9f3561ee15ee4afbf0150617f9b9b75db8a5e73
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-19 12:34:25 +0300

    extra spaces in output removed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ with urllib.request.urlopen(req) as resp:
     body = resp.read()
 
 print("Body response:")
-print("    - type: {}".format(type(body)))
-print("    - content: {}".format(body))
-print("    - utf8 content: {}".format(body.decode('utf-8')))
+print(" - type: {}".format(type(body)))
+print(" - content: {}".format(body))
+print(" - utf8 content: {}".format(body.decode('utf-8')))
