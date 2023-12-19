c83b91d6dc52e63bddae4dcf96e6053d26fdcfc2
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-19 12:01:00 +0300

    looking at respose attributes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """  Module for sending HTTP request"""
 import urllib.request
+#import chardet
 
-# req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen('http://alx-intranet.hbtn.io/status') as resp:
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as resp:
    body = resp.read()
+
 print("Body response:")
 print("    - type: {}".format(type(body)))
 print("    - content: {}".format(body))
-print("    - utf8 content: OK")
-
+print("    - utf8 content: {}".format(ok))
+print()
