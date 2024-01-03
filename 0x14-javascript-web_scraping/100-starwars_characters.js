2a20f3c20fa2c349c374f6990271cd9dc51739e4
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-04 00:22:23 +0300

    characters in movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,3 +1,24 @@
 #!/usr/bin/node
 
-star_wrs = 'https://swapi-api.alx-tools.com/api/'
+request = require('request');
+movieId = process.argv[2];
+star_wrs = 'https://swapi-api.alx-tools.com/api/';
+
+let characters = [];
+request (star_wrs, function (error, response, body) {
+  let bodyResp = JSON.parse(body);
+    
+    let url = bodyResp.people
+    request (url, function (error, response, body) {
+      let Resp = JSON.parse(body);
+	/*console.log(Resp);*/
+
+      for (count = 0; count < Resp.results.length; count++) {
+	 characters.push(Resp.results[count].name);
+      }
+      for (nameCount = 0; nameCount < characters.length; nameCount++) {
+	 console.log(characters[nameCount]);
+      } 
+    });
+});
+
