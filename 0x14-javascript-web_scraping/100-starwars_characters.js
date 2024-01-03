deacc4d1fc4ba25b822153d1c2fdaf40c6e450b5
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-04 00:51:25 +0300

    character development

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,24 +1,30 @@
 #!/usr/bin/node
 
-request = require('request');
-movieId = process.argv[2];
-star_wrs = 'https://swapi-api.alx-tools.com/api/';
+const request = require('request');
+/* const movieId = process.argv[2]; */
+const starWrs = 'https://swapi-api.alx-tools.com/api/';
 
-let characters = [];
-request (star_wrs, function (error, response, body) {
-  let bodyResp = JSON.parse(body);
-    
-    let url = bodyResp.people
-    request (url, function (error, response, body) {
-      let Resp = JSON.parse(body);
-	/*console.log(Resp);*/
+const characters = [];
 
-      for (count = 0; count < Resp.results.length; count++) {
-	 characters.push(Resp.results[count].name);
-      }
-      for (nameCount = 0; nameCount < characters.length; nameCount++) {
-	 console.log(characters[nameCount]);
-      } 
-    });
+request(starWrs, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const bodyResp = JSON.parse(body);
+
+  const url = bodyResp.people;
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    const Resp = JSON.parse(body);
+    /* console.log(Resp); */
+
+    for (let count = 0; count < Resp.results.length; count++) {
+      characters.push(Resp.results[count].name);
+    }
+    for (let nameCount = 0; nameCount < characters.length; nameCount++) {
+      console.log(characters[nameCount]);
+    }
+  });
 });
-
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,3 +1,29 @@
 #!/usr/bin/node
 
-star_wrs = 'https://swapi-api.alx-tools.com/api/'
+const request = require('request');
+/* const movieId = process.argv[2]; */
+const starWrs = 'https://swapi-api.alx-tools.com/api/';
+
+const characters = [];
+request(starWrs, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const bodyResp = JSON.parse(body);
+
+  const url = bodyResp.people;
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    const Resp = JSON.parse(body);
+    /* console.log(Resp); */
+
+    for (let count = 0; count < Resp.results.length; count++) {
+      characters.push(Resp.results[count].name);
+    }
+    for (let nameCount = 0; nameCount < characters.length; nameCount++) {
+      console.log(characters[nameCount]);
+    }
+  });
+});
