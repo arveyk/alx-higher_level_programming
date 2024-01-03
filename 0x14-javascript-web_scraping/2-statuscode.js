e4ae4377e2f4ccd551460d589a3e5c14d70467c4
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-03 16:21:43 +0300

    deprecated module absent

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+url = process.argv[2]
+
+const request = require('request');
+request(url, function (error, response, body) {
+  console.error('error', error);
+  console.log('statusCode:', response && response.statusCode);
+});
