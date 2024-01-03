ce9db1a0eef36d629c0c51dc09c4655ecc7d3366
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-03 17:23:54 +0300

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,25 +1,11 @@
 #!/usr/bin/node
 
 const url = process.argv[2];
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const request = require('request');
 
 request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log('code:', response && response.statusCode);
 });
-
-/*
-fetch(url)
-	.then((response) => response.text())
-	.then((data) => {
-	    console.log(data);
-	})
-	.catch((error) => {
-	    console.error(error);
-	});
-	*/
-
-/*
- * const response = fetch(url);
-console.log(response.status);
-*/
