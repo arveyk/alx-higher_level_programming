9d9fe6e89c4f151096e8357a0e6bdbef6f942ece
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-27 19:48:59 +0300

    inheriting from list, priting sorted list without modifying the list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,10 +2,15 @@
 """Module to demo inheriring from list
 """
 
+
 class MyList(list):
-    """Inerits from list
     """
-    def print_sorted(self):
-        """Prints the inhertited sorted list
-        """
-        print(list.sort)
+    Inherits from list
+    """
+
+    def __init__(self, list=[]):
+        pass
+
+    def print_sorted(list=[]):
+        new_list = list
+        print(sorted(new_list))
