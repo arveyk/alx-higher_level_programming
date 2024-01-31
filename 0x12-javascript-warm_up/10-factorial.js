3808ed338f396e4723d46bba61040bfde1b14434
Harvey W. Kis <harveykisiangani@gmail.com>
2024-02-01 00:03:17 +0300

    semistandard check

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
 
-let number = num;
+const number = num;
 
 if (isNaN(num)) {
   console.log(1);
 } else {
-  function fact(first) {
-    if (first == 1) {
+  function fact (first) {
+    if (first === 1) {
       return (1);
     }
     return (first * fact(first - 1));
