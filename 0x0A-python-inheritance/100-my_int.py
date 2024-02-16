ee6e178a40aae39ae6eece91d78a6521e4e6f907
Harvey W. Kis <harveykisiangani@gmail.com>
2024-02-16 14:26:55 +0300

    comparison logic corrected

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,7 +8,10 @@ class MyInt(int):
     inverts ==  and != opertators
     """
     def __init__(self, other):
+        """Initalizes the class attributes
+        """
         def __eq__(self, other):
-            return (self.a != other.b)
+            return (self.a == other.b)
+
         def __ne__(self, other):
-            return self.a == other.a
+            return self.a != other.a
