3444eb2795adb2635ed862283d7f92595a1118c6
Harvey W. Kis <harveykisiangani@gmail.com>
2024-02-16 13:56:50 +0300

    correct syntax for inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     '''
     def __init__(self, size):
         super().__init__(size, size)
-        Rectangle.super().integer_validator('size', size)
+        Rectangl.integer_validator('size', size)
         self.__size = size
 
     def area(self):
