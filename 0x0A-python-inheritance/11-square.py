18ce01c785cb2229daaa254af1a73f492f621d56
Harvey W. Kis <harveykisiangani@gmail.com>
2024-02-16 13:58:30 +0300

    correct parent class name

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     '''
     def __init__(self, size):
         super().__init__(size, size)
-        Rectangl.integer_validator('size', size)
+        Rectangle.integer_validator('size', size)
         self.__size = size
 
     def area(self):
