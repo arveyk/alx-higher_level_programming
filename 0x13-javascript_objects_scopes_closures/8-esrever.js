d8940a1e86fb0d799054c95b2a1e098e091a7474
Harvey W. Kis <harveykisiangani@gmail.com>
2024-03-15 16:23:38 +0300

    exports function

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const esrever = function (list) {
+exports.esrever = function (list) {
   const listRev = [];
   for (let len = list.length - 1; len >= 0; len--) {
     listRev.push(list[len]);
   }
   return (listRev);
 };
-exports.esrever = esrever;
