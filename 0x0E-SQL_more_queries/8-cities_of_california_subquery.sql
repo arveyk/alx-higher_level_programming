8c080c98fcba6072f836ccff84c8e25ac248f020
Harvey W. Kis <harveykisiangani@gmail.com>
2024-03-18 13:58:46 +0300

    in vs = in subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 SELECT cities.id, name
 FROM cities
 WHERE 
-	cities.id IN (SELECT state_id FROM states
+	cities.id = (SELECT state_id FROM states
 		WHERE states.name = 'California')
 ORDER BY id ASC;
