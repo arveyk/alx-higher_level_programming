44e5b837d954e2812f64703c8bd15f14ee002b11
Harvey W. Kis <harveykisiangani@gmail.com>
2024-04-05 17:38:32 +0300

    Documentation added

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,6 +4,8 @@ import sys
 
 count = 0;
 try:
+    """ receive IP address info an prints out activies
+    from users of given IP"""
     report = sys.stdin
     for line in report:
         if (count % 10):
@@ -11,4 +13,5 @@ try:
         print(': {}'.format(line[-2:]))
         count += 1
 except KeyboardInterrupt:
+    """ Captures the keyboard interrupt """
     print('File size:{} {}'.format(line[0:5], len(line)))
