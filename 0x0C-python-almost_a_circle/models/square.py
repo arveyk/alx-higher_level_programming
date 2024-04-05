5a1098fdb3310b28d458a00bcb1a0609afc9b1ae
Harvey W. Kis <harveykisiangani@gmail.com>
2024-04-05 18:15:04 +0300

    initializing super with correct number of argument

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,28 +3,28 @@
 
 
 from models.rectangle import Rectangle
-from models.base import Base
 
-class Square(Rectangle, Base):
-    """ Defines a square, similar to a rectangle but 
+
+class Square(Rectangle):
+    """ Defines a square, similar to a rectangle but
         with width == height
     """
     def __init__(self, size, x=0, y=0, id=None):
-        self.__width = size
-        self.__height = size
-        self.__x = x
-        self.__y = y
-        Rectangle().__init__(id, width, height, x=0, y=0)
-        Base().__init__(id, width, height, x=0, y=0)
+        super().__init__(size, size, x, y, id)
+        self.__size = size
+
 
     def __str__(self):
-        return f'[{Square}]'
+        return f'[{Square.__name__}] {self.id} {self.x}/{self.y} - {self.size}'
 
     @property
     def size(self):
+        """Getter: returns the size attribute
+        Setter: modifies the value of the size attribut
+        """
         return self.__size
 
     @size.setter
     def size(self, size):
-        self.width = size
-        self.height = size
+        self.__width = size
+        self.__height = size
