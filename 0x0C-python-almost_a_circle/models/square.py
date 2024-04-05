d782df4f5959ac7e7680e21c99e76cfe038d3f3d
Harvey W. Kis <harveykisiangani@gmail.com>
2024-04-05 18:16:12 +0300

    pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,6 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.__size = size
 
-
     def __str__(self):
         return f'[{Square.__name__}] {self.id} {self.x}/{self.y} - {self.size}'
 
