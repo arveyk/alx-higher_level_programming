6fc4b66b4d6d52e049654f82c7dada17b6eca639
arveyk <harveykisiangani@gmail.com>
2024-12-20 21:22:14 +0300

    Selection by state_id in cities column

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 SELECT cities.id, name
 FROM cities
 WHERE 
-	cities.id = (SELECT state_id FROM states
+	cities.state_id = (SELECT id FROM states
 		WHERE states.name = 'California')
 ORDER BY id ASC;
