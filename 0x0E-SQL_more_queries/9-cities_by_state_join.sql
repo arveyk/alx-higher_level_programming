693a2332c250fb84a3785ffcbc7185a34315f1ec
arveyk <harveykisiangani@gmail.com>
2024-12-20 21:36:14 +0300

    Join condition added

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,6 @@
 -- FROM cities INNER JOIN states
 -- WHERE states.name = 'California'
 -- ORDER BY id ASC;
-SELECT cities.id ,cities.name , states.name FROM cities INNER JOIN states
+SELECT cities.id ,cities.name, states.name FROM cities INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
