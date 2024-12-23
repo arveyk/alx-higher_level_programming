fb922f1f6e44e4ecee110e4a6d8b05a5c71f72eb
arveyk <harveykisiangani@gmail.com>
2024-12-23 22:12:14 +0300

    Arranged in Ascending order

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,7 @@
 -- Script to list all Dexter genres
-SELECT tv_genres.name FROM tv_genres
+SELECT name FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id=tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_shows.title='DEXTER';
+WHERE tv_shows.title='DEXTER'
+ORDER BY name ASC;
