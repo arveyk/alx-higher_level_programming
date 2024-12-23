be752d1dfde8d6efca8909e738fd7cf9b4ca6c14
arveyk <harveykisiangani@gmail.com>
2024-12-23 20:27:18 +0300

    No genres = where col values are null

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,6 @@
 -- Script to display all shows
-SELECT tv_show.title, tv_shows_genres.genre_id
-FROM hbtn_0d_tvshows
-ORDER BY tv_shows.title ASC tv_shows_genres.genre_id DESC;
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, genre_id ASC;
