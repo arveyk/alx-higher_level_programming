0ef4e8df668e00e976345369757d5e0ce7067fc5
arveyk <harveykisiangani@gmail.com>
2024-12-23 21:55:23 +0300

    Column alias

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script that counts
-SELECT tv_genres.name, count(*) AS number_of_shows FROM tv_genres
+SELECT tv_genres.name genre, count(*) AS number_of_shows FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id=tv_genres.id
 WHERE tv_show_genres.genre_id IS NOT NULL
