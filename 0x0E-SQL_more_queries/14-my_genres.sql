98869eed4f2b1725ee2b4d8bb7547b26c80db00f
arveyk <harveykisiangani@gmail.com>
2024-12-23 22:04:57 +0300

    Join statement for multiple tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Script to list all Dexter genres
-SELECT tv_genres.name FROM tv_show_genres
-INNER JOIN tv_genres
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres
 ON tv_genres.id=tv_show_genres.genre_id
-WHERE show_id=(SELECT id FROM tv_shows
-	WHERE title='DEXTER');
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title='DEXTER';
