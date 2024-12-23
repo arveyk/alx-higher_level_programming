69c7d0dd52a00533391af8421422ab0ddc55c0e1
arveyk <harveykisiangani@gmail.com>
2024-12-23 21:38:44 +0300

    Non-linked genres removed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,6 @@
 SELECT tv_genres.name, count(*) AS number_of_shows FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY tv_genres.name
-ORDER BY number_of_shows DESC ;
+ORDER BY number_of_shows DESC;
