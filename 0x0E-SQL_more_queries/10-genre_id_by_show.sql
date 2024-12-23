9dc5b19eb22fd25906639474fb1df73b7b66f6a2
arveyk <harveykisiangani@gmail.com>
2024-12-23 20:04:48 +0300

    Left join exclude null values

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
 -- Script to display all shows
-SELECT title, tv_show_genres.genre_id AS genre_id
-FROM tv_shows CROSS JOIN tv_show_genres
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NOT NULL
 ORDER BY title, genre_id ASC;
