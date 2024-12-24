bbe6ceab953ff818d28008a7fe8f242e531129af
arveyk <harveykisiangani@gmail.com>
2024-12-24 19:47:26 +0300

    Syntax error, extra closing quotation mark after statement termination

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,4 +5,4 @@ SELECT DISTINCT tv_shows.title
 FROM tv_show_genres LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id 
 INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE name='Comedy'
-ORDER BY title;" 
+ORDER BY title; 
