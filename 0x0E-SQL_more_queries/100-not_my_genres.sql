88527d383d5627dd5cced31d40fdb964f9c5e282
arveyk <harveykisiangani@gmail.com>
2024-12-24 19:11:34 +0300

    2 Queries, One EXCEPTion

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,7 @@
 -- Script to list all Dexter genres
 SELECT name FROM tv_genres
-EXCEPT
-SELECT DISTINCT name FROM tv_show_genres
+EXCEPT SELECT DISTINCT name FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
-WHERE show_id=8
+LEFT JOIN tv_shows on tv_shows.id=tv_show_genres.show_id 
+WHERE title='Dexter'
 ORDER BY name;
