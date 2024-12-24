127745f5161c9ce26d543025c847b3da9ae4b88c
arveyk <harveykisiangani@gmail.com>
2024-12-24 14:05:55 +0300

    Right join

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Script to display shows by genre
+SELECT tv_shows.title title, tv_genres.name FROM tv_show_genres
+RIGHT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+ORDER BY title;
