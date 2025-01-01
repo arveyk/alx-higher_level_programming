75a591a7ba7bbaf7fdba6b6bf95461fcf0d54921
arveyk <harveykisiangani@gmail.com>
2025-01-01 20:48:45 +0300

    Binary for case sensitive search

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,15 +8,19 @@ if __name__ == '__main__':
     if len(sys.argv) == 4:
         argv = sys.argv
         usrname = argv[1]
-        password = argv[2]
+        pwrd = argv[2]
         db_name = argv[3]
 
-        db = MySQLdb.connect(user=usrname, passwd=password, db=db_name)
+        db = MySQLdb.connect(user=usrname, passwd=pwrd, db=db_name)
         c_ursor = db.cursor()
-        qry = ("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+        qry = ("SELECT * FROM states\
+               WHERE name LIKE BINARY 'N%' ORDER BY states.id")
         c_ursor.execute(qry)
 
         rows = c_ursor.fetchall()
 
         for echR in rows:
             print(echR)
+
+        cursor.close()
+        db.close()
