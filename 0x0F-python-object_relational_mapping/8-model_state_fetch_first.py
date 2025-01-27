417c596ebf3d4d2dc619f3f6c26d800f7d9be2b5
arveyk <harveykisiangani@gmail.com>
2025-01-27 23:07:46 +0300

    Query First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Script using sqlalchemy to demo ORM
+"""
+
+
+from model_state import Base, State
+from string import Template
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session, sessionmaker
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        '''for rows in session.query(State).order_by(State.id).one():
+            print("{}: {}".format(rows.id, rows.name))
+            '''
+        first_state = session.query(State).first()
+        print("{}: {}".format(first_state.id, first_state.name))
