bb55789a3262fcb167f9278cfbf2dca8b3fab380
arveyk <harveykisiangani@gmail.com>
2025-02-15 20:48:58 +0300

    pycodestyle

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''Ger response header
 '''
-import requests, sys
+import requests
+import sys
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
     r = requests.get(url)
     print(r.headers['X-Request-id'])
-
