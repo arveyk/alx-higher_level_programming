dbe1d3919610a0e0cf876adc97b544914b6fee80
arveyk <harveykisiangani@gmail.com>
2025-02-15 22:23:08 +0300

    Removed extra print statement

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,6 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     r = requests.get(url)
-    print(r.headers.get('X-Request-id'))
     if r.status_code > 399:
         print('Error code: {}'.format(r.status_code))
     else:
