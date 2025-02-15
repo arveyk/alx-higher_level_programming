fc284dcbb967d41441c12c7208aaea66c7449341
arveyk <harveykisiangani@gmail.com>
2025-02-15 22:03:10 +0300

    r for request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ import sys
 if __name__ == '__main__':
     usremail = {'email': sys.argv[2]}
     url = sys.argv[1]
-    ir = requests.post(url, data=usremail)
+    r = requests.post(url, data=usremail)
     print(r.text)
