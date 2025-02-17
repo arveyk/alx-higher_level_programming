eebe12933e33153e146ebe4cf764799951509575
arveyk <harveykisiangani@gmail.com>
2025-02-17 16:38:46 +0300

    pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-''' Script to list github commits
+''' Script to retieve github commits
+based on the user name and repo name
 '''
 import requests
 import sys
 
-repo = sys.argv[1]
-name = sys.argv[2]
-
-url = 'https://api.github.com/repos/{}/{}/commits'.format(
+if __name__ == '__main__':
+    '''Where the action is. The above statement ensures that
+    the script is not run when imported
+    Args:
+        from the command line
+        first : repo name
+        second: user name
+    '''
+    repo = sys.argv[1]
+    name = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
         name, repo)
-resp = requests.get(url)
-Json_R = resp.json()
-
-for i in range(10):
-    print('{}: {}'.format(Json_R[i].get('sha'),
-                          Json_R[i]['commit']['author']['name']))
+    resp = requests.get(url)
+    Json_R = resp.json()
+    for i in range(10):
+        print('{}: {}'.format(Json_R[i].get('sha'),
+                              Json_R[i]['commit']['author']['name']))
